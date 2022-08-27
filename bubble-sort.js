e67396df8e5d4d425b58941474630b24d843acf2
Krishnakanta Naik <krishnakantanaik04@gmail.com>
2022-08-27 19:15:34 +0530

    Bubble Sort

---
+++ bubble-sort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j + 1] < arr[j]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.table(bubbleSort([5, 4, 3, 2, 1]));
